e7a8131c34374ef21157d7b39e42acc0af820066
Pavel <pvdymov@ya.ru>
2018-08-13 22:34:59 +0700

    добавлен фон и еще картинка)

--- core/src/ru/gb/stargame/StarGame.java
+++ core/src/ru/gb/stargame/StarGame.java
@@ -7,27 +7,33 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class StarGame extends ApplicationAdapter {
-	private SpriteBatch batch;
-	private Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(10,10,10,1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+    private SpriteBatch batch;
+    private Texture img;
+    private Texture background;
+
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        img = new Texture("pepelaz.png");
+        background = new Texture("midgard.png");
+    }
+
+    @Override
+    public void render() {
+        Gdx.gl.glClearColor(1, 1, 1, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        batch.begin();
+        batch.draw(background, 0, 0);
+        batch.draw(img, 0, 0);
+        batch.end();
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        img.dispose();
+    }
+
 }
--- desktop/src/ru/gb/stargame/desktop/DesktopLauncher.java
+++ desktop/src/ru/gb/stargame/desktop/DesktopLauncher.java
@@ -5,8 +5,13 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import ru.gb.stargame.StarGame;
 
 public class DesktopLauncher {
+
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "StarGame";
+		config.height = 450;
+		config.width = 800;
 		new LwjglApplication(new StarGame(), config);
 	}
+
 }

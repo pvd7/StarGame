458f4e87f7fb50f19eabc0b85ac0d8113c2b14b7
Pavel Dymov <pvdymov@vankoroil.ru>
2018-08-22 11:37:49 +0700

    fix all

--- desktop/src/ru/gb/stargame/desktop/DesktopLauncher.java
+++ desktop/src/ru/gb/stargame/desktop/DesktopLauncher.java
@@ -2,16 +2,16 @@ package ru.gb.stargame.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import ru.gb.stargame.Star2DGame;
+
+import ru.geekbrains.stargame.Star2DGame;
 
 public class DesktopLauncher {
-
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.title = "StarGame";
-//		config.height = 1960;
-//		config.width = 1080;
+		float aspect = 3f/4f;
+		config.width= 350;
+		config.height = (int) (config.width / aspect);
+		config.resizable = false;
 		new LwjglApplication(new Star2DGame(), config);
 	}
-
 }
